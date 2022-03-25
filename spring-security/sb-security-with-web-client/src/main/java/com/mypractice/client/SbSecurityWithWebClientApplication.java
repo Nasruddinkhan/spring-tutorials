f4fcc5f02add9bc165f006fed17451583a1f4c2c
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-03-26 02:13:39 +0530

    add web aouth2 client

---
+++ spring-security/sb-security-with-web-client/src/main/java/com/mypractice/client/SbSecurityWithWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.mypractice.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityWithWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityWithWebClientApplication.class, args);
+	}
+
+}
---
+++ spring-security/sb-security-with-web-client/src/test/java/com/mypractice/client/sbsecuritywithwebclient/SbSecurityWithWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypractice.client.sbsecuritywithwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityWithWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
