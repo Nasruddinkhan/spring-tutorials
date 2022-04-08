6d455a656f36b3dd11a3da7e2babafad8554fbde
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-04-09 00:52:03 +0530

    depedency lookup example ]

---
+++ spring-core/ioc/spring-bean-traditional-dependencylookup-ac/src/main/java/com/mypractice/test/DependencyLookupTest.java
@@ -0,0 +1,27 @@
+package com.mypractice.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mypractice.beans.Viechle;
+
+public class DependencyLookupTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=null;
+	    Viechle viechle=null;
+		//create IOC container
+	    ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+		System.out.println("IOC container of Client App is created");
+		//get Target class object
+		viechle=ctx.getBean("viechle",Viechle.class);
+		//invoke methods
+		viechle.journey("mumbai","pune");
+		viechle.entertainment();
+		viechle.soundHorn();
+		//close container
+		((AbstractApplicationContext) ctx).close();
+	}
+
+}
