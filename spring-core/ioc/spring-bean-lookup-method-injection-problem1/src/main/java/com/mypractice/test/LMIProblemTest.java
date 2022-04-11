74a32ae2d1efc366a163475a03dc6b38b604a667
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-04-12 00:18:51 +0530

    add the method injectiotion solution

---
+++ spring-core/ioc/spring-bean-lookup-method-injection-problem1/src/main/java/com/mypractice/test/LMIProblemTest.java
@@ -0,0 +1,30 @@
+package com.mypractice.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mypractice.beans.MyServletContainer;
+
+public class LMIProblemTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=null;
+		MyServletContainer container=null,container1=null;
+		//create IOC container
+		ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+		//get Taget Bean class obj
+		container=ctx.getBean("container",MyServletContainer.class);
+		container1=ctx.getBean("container",MyServletContainer.class);
+		//invoke methods
+		container.processRequest("seven wonders");
+		System.out.println(".................................");
+		container1.processRequest("ameerpet");
+		
+		
+		//close container
+		((AbstractApplicationContext) ctx).close();
+
+	}
+
+}
---
+++ spring-core/ioc/spring-bean-lookup-method-injection-solution1/src/main/java/com/mypractice/test/LMISolutonTest.java
@@ -0,0 +1,31 @@
+package com.mypractice.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mypractice.beans.MyServletContainer;
+
+public class LMISolutonTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");;
+		MyServletContainer container=null,container1=null;
+		//create IOC container
+		//ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+		//get Taget Bean class obj
+		container=ctx.getBean("container",MyServletContainer.class);
+		container1=ctx.getBean("container",MyServletContainer.class);
+		//container1.processRequest("seven wonders");
+		//invoke methods
+		//container.processRequest("seven wonders");
+		System.out.println(".................................");
+	//	container1.processRequest("ameerpet");
+
+
+		//close container
+		((AbstractApplicationContext) ctx).close();
+
+	}
+
+}
